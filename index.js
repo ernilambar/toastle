7cfcb3c8214aa72f9c99abab6abb018053c11bc0
Nilambar Sharma <nilambar@outlook.com>
2025-09-15 05:27:48 +0545

    Reduce spacing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Toastle = options => {
 
   // Calculate position based on existing toasts.
   const toastHeight = 40; // Approximate height including margin.
-  const spacing = 5;
+  const spacing = 3;
   const currentTop = top + activeToasts.length * (toastHeight + spacing);
 
   notice.style.position = 'fixed';
